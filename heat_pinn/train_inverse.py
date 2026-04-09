32dbd6834ceb0ce229ba66772b8b589eccd63a65
loreogh <lorenzo.oghittu@gmail.com>
2026-04-09 11:21:05 +0200

    Solving inverse problem with heat equation

--- heat_pinn/train_inverse.py
+++ heat_pinn/train_inverse.py
@@ -13,12 +13,22 @@ n_pde = 1000
 
 N_epoch = int(1e4)
 
+alpha_guess = 0.5
+alpha_real = 0.1
+
+# Sampling
+n_samp = 100
+x_samp = torch.rand(n_samp, 1)
+t_samp = torch.rand(n_samp, 1)
+u_samp = torch.exp(-alpha_real * torch.pi**2 * t_samp) * torch.sin(torch.pi * x_samp)
+xt_samp = torch.cat([x_samp, t_samp], dim=1)
+
+
 # Loss parameters
 lambda_ic = 10.0
 lambda_bc = 10.0
+lambda_data = 10.0
 
-# Physical parameters
-alpha = 0.1
 
 # Loss functions
 def loss_pde(model, x, t, xt, alpha):
@@ -70,25 +80,39 @@ def loss_bc2(model, n_points = n_cond):
 
     return torch.mean(u**2)
 
+
+def loss_data(model, u_samp, xt_samp):
+
+    u = model(xt_samp)
+
+    return torch.mean((u - u_samp)**2)
+
+
 # Model
 model = MLP(n_in, n_out, n_hidden, n_layers)
 
-# Optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+# Random points
+alpha = torch.nn.Parameter(torch.tensor([alpha_guess]))  
 
-# Points
 x = torch.rand(n_pde, 1).requires_grad_(True)
 t = torch.rand(n_pde, 1).requires_grad_(True)
 xt = torch.cat([x, t], dim=1)
+
+# Optimizer
+optimizer = torch.optim.Adam([
+    {'params': model.parameters(), 'lr': 1e-3},
+    {'params': [alpha], 'lr': 1e-2}  # different lr for alpha
+], )
 
 # Optimization loop
 for epoch in range(N_epoch):
 
     optimizer.zero_grad()
     loss_total = (loss_pde(model, x, t, xt, alpha) 
-                + lambda_ic * loss_ic(model) 
-                + lambda_bc * loss_bc1(model) 
-                + lambda_bc * loss_bc2(model))
+            + lambda_ic * loss_ic(model)
+            + lambda_bc * loss_bc1(model)
+            + lambda_bc * loss_bc2(model)
+            + lambda_data * loss_data(model, u_samp, xt_samp))
     loss_total.backward()
     optimizer.step()
 
@@ -97,22 +121,6 @@ for epoch in range(N_epoch):
         l_ic = loss_ic(model)
         l_bc1 = loss_bc1(model)
         l_bc2 = loss_bc2(model)
-        print(f"Epoch {epoch} | PDE: {l_pde.item():.2e} | IC: {l_ic.item():.2e} | BC1: {l_bc1.item():.2e} | BC2: {l_bc2.item():.2e}")
+        print(f"Epoch {epoch} | alpha: {alpha.item():.4f} | PDE: {l_pde.item():.2e} | IC: {l_ic.item():.2e} | BC1: {l_bc1.item():.2e} | BC2: {l_bc2.item():.2e}")
 
 
-# Saving 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-save_path = os.path.join(script_dir, 'heat_pinn.pth')
-
-torch.save({
-    'model_state_dict': model.state_dict(),
-    'model_params': {
-        'n_in': n_in,
-        'n_out': n_out,
-        'n_hidden': n_hidden,
-        'n_layers': n_layers
-    },
-    'physics_params': {
-        'alpha': alpha
-    }
-}, save_path)
